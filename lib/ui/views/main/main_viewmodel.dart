47be37d8016174eeb2003f117608e49efae879b5
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-07-29 12:10:15 +0800

    ...

--- lib/ui/views/main/main_viewmodel.dart
+++ lib/ui/views/main/main_viewmodel.dart
@@ -1,3 +1,16 @@
 import 'package:stacked/stacked.dart';
 
-class MainViewModel extends BaseViewModel {}
+enum MenuType {
+  LOGIN,
+  REGISTER,
+}
+
+class MainViewModel extends BaseViewModel {
+  MenuType _menuType;
+  MenuType get menuType => _menuType;
+
+  selectMenu({MenuType menuType}) {
+    _menuType = menuType;
+    notifyListeners();
+  }
+}
