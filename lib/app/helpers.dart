e845985117f5e8ce5be45b5dc3ffa6c5bbbd43f5
Carlo Miguel Dy <45052332+carlomigueldy@users.noreply.github.com>
2020-07-31 21:23:13 +0800

    Update helpers.dart

--- lib/app/helpers.dart
+++ lib/app/helpers.dart
@@ -1,10 +1,7 @@
 import 'package:dio/dio.dart';
 
 // ignore: non_constant_identifier_names
-// final String API_BASE_URL = 'http://192.168.1.7';
-
-// ignore: non_constant_identifier_names
-final String API_BASE_URL = 'https://app.carlomigueldy.com';
+final String API_BASE_URL = 'http://192.168.1.7';
 
 /// Dio HTTP client, accessible globally with a single [API_BASE_URL]
 ///
